dc2b946e9a8570c53740a1c0ff0038e2e450bf12
tamjiahao0417 <tamhao@graduate.utm.my>
2025-06-07 13:01:24 +0800

    udpate lec frontend

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,17 @@ import LoginPage from '../views/LoginPage.vue'
 import ManageStudentEnrollmentPage from '../views/ManageStudentEnrollmentPage.vue'
 import ComponentMarksPage from '../views/ComponentMarksPage.vue'
 import AddFinalExamMarksPage from '../views/AddFinalExamMarksPage.vue'
-
+import ViewFullMarkBreakdownPage from '../views/ViewFullMarkBreakdownPage.vue'
+import AddPerformanceTrendPage from '../views/AddPerformanceTrendPage.vue'
 
 const routes = [
   { path: '/home', name: 'Home', component: HomePage },
   { path: '/', name: 'Login', component: LoginPage },
   { path: '/enroll', name: 'Enroll', component: ManageStudentEnrollmentPage },
   { path: '/manage-component-marks', name: 'ComponentMarks', component: ComponentMarksPage },
-  { path: '/add-final-exam-marks', name: 'AddFinalExamMarks', component: AddFinalExamMarksPage }
-
+  { path: '/add-final-exam-marks', name: 'AddFinalExamMarks', component: AddFinalExamMarksPage },
+  { path: '/mark-breakdown', name: 'ViewFullMarkBreakdown', component: ViewFullMarkBreakdownPage },
+  { path: '/performance-trend', name: 'AddPerformanceTrend', component: AddPerformanceTrendPage }
 ]
 
 
